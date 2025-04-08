fde7c4853255fbcd65523e7c7d0098cfcd1364de
0xAgun <islamtahsan0@gmail.com>
2025-04-08 19:22:42 +0600

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+source ~/.config/fish/user_variables.fish
+source ~/.config/fish/abbreviations.fish
+alias burp='"/usr/lib/jvm/java-21-openjdk/bin/java" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:/home/agun/burp/BurpLoaderKeygen_v1.17.jar" "-noverify" "-jar" "/home/agun/burp/burpsuite_pro_v2025.2.3.jar"'
+
